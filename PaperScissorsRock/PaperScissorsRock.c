278d38fb74c8c794643ffdda73e9fa709a602a70
Wobz <jackbeaker@gmail.com>
2020-10-03 11:18:35 +1300

    Pause working

--- PaperScissorsRock/PaperScissorsRock.c
+++ PaperScissorsRock/PaperScissorsRock.c
@@ -10,15 +10,13 @@
 #define PACER_RATE 500
 #define MESSAGE_RATE 15
 
-static const pio_t rows[] =
-{
-    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO, 
+static const pio_t rows[] = {
+    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
     LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
     LEDMAT_ROW7_PIO
 };
 
-static const pio_t cols[] =
-{
+static const pio_t cols[] = {
     LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
     LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
 };
@@ -27,18 +25,18 @@ int navswitch_moved(void)
 {
     int moved = 0;
     for (int i = 0; i < 5; i++) {
-		
-    
-		if (navswitch_push_event_p (i)) {
-			moved = 1;
-		}
-	}
-	return moved;
+
+
+        if (navswitch_push_event_p (i)) {
+            moved = 1;
+        }
+    }
+    return moved;
 }
 
 void text_init(void)
 {
-	tinygl_init(PACER_RATE);
+    tinygl_init(PACER_RATE);
     tinygl_font_set(&font3x5_1);
     tinygl_text_speed_set(MESSAGE_RATE);
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
@@ -47,39 +45,47 @@ void text_init(void)
 
 void display_message_until_joystick_moved(char* message)
 {
-	tinygl_text(message);
-	int moved = 0;
-	while (moved != 1)
-	{
-		pacer_wait();
-		tinygl_update();
-		navswitch_update();
-		moved = navswitch_moved();
-	}
-	for (int row = 0; row < 7; row++) {
-		pio_config_set(rows[row], PIO_OUTPUT_HIGH);
-	}
-	
-	for (int col = 0; col < 5; col++) {
-		pio_config_set(cols[col], PIO_OUTPUT_HIGH);
-	}
-	
+    tinygl_text(message);
+    int moved = 0;
+    while (moved != 1) {
+        pacer_wait();
+        tinygl_update();
+        navswitch_update();
+        moved = navswitch_moved();
+    }
+    for (int row = 0; row < 7; row++) {
+        pio_config_set(rows[row], PIO_OUTPUT_HIGH);
+    }
+
+    for (int col = 0; col < 5; col++) {
+        pio_config_set(cols[col], PIO_OUTPUT_HIGH);
+    }
+
 }
 
- int main(void)
+void pause(uint16_t time)
+{
+    uint16_t tick = 0;
+    while (tick < time) {
+        tick++;
+        pacer_wait();
+    }
+}
+
+int main(void)
 {
     system_init ();
     navswitch_init();
-	text_init();
-	
-	pacer_init(PACER_RATE);
-	display_message_until_joystick_moved("WELCOME TO PAPER SCISSORS ROCK");
-	display_message_until_joystick_moved("SELECT");
-	
-	
-    while (1)
-    {
-		pacer_wait();
+    text_init();
+
+    pacer_init(PACER_RATE);
+    display_message_until_joystick_moved("WELCOME TO PAPER SCISSORS ROCK");
+    pause(1000);
+    display_message_until_joystick_moved("SELECT");
+
+
+    while (1) {
+        pacer_wait();
 
     }
 }
